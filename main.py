9ea5008ec06870dd124a9bc830bd8ab9110051e5
elngbr <eftimieelena22@stud.ase.ro>
2024-02-12 01:04:06 +0200

    1st commit. See the file. That's way mooooooooooooore easier than importing all those libraries

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+sns.set_style("whitegrid")
+plt.style.use("fivethirtyeight")
+
+# example text for model training (SMS messages)
+simple_train = ['call you tonight', 'Call me a cab', 'Please call me... PLEASE!']
+
+vect=CountVectorizer()
+vect.fit(simple_train)
+print 
+print(vect.get_feature_names_out())
