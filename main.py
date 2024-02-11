b1fb3a2fda8e2bd8e3d73a4a1ab0d882a4d54db7
elngbr <eftimieelena22@stud.ase.ro>
2024-02-12 01:13:07 +0200

    2nd commit. Examining the vocabulary to see if recognizises words

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 from sklearn.feature_extraction.text import CountVectorizer
+
 
 sns.set_style("whitegrid")
 plt.style.use("fivethirtyeight")
@@ -11,7 +11,16 @@ plt.style.use("fivethirtyeight")
 # example text for model training (SMS messages)
 simple_train = ['call you tonight', 'Call me a cab', 'Please call me... PLEASE!']
 
-vect=CountVectorizer()
+vect = CountVectorizer()
 vect.fit(simple_train)
-print 
 print(vect.get_feature_names_out())
+
+# transform training data into a 'document-term matrix'
+simple_train_dtm = vect.transform(simple_train)
+print(simple_train_dtm)
+# convert sparse matrix to a dense matrix
+print(simple_train_dtm.toarray())
+
+# examine the vocabulary and document-term matrix together
+df = pd.DataFrame(simple_train_dtm.toarray(), columns=vect.get_feature_names_out())
+print(df)
